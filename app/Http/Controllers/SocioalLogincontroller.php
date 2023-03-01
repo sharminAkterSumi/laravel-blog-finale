6d27ba1a2390df473875ec888f150d739b747194
Sharmin Akter Sumi <116646694+sharminAkterSumi@users.noreply.github.com>
2023-03-01 00:49:36 -0800

    socil

--- app/Http/Controllers/SocioalLogincontroller.php
+++ app/Http/Controllers/SocioalLogincontroller.php
@@ -10,6 +10,11 @@ class SocioalLogincontroller extends Controller
 {
  public function googlelogin()
  {
+  /**
+   * GOOGLE_CLIENT_ID=960911124865-ij0jdvuao7nar6m4faet2dl85fqf7n58.apps.googleusercontent.com
+*GOOGLE_CLIENT_SECRET=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
+*GOOGLE_REDIRECT_URL= http://localhost:8000/google/redirect
+   */
   // return Socialite::driver('google')->redirect();
  }
  public function googleredirect()
